e1caffda6a049fbced042df25e8058dc63d4943a
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-07-18 09:54:43 +0400

    [Lesson-16] added store

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,6 +8,27 @@
       <button>Refresh</button>
       <router-link to='/register'>Register as Coach</router-link>
     </div>
-    <ul>LIST OF COACHES</ul>
+    <ul v-if='hasCoaches'>
+      <li v-for='(coach) in filteredCoaches' :key='coach.id'>
+        {{coach.firstName}}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+import { charAt } from 'core-js/internals/string-multibyte';
+
+export default {
+  methods: { charAt },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import {createStore} from 'vuex';
+import coachesModule from './modules/coaches';
+
+export default createStore({
+  modules: {
+    coaches: coachesModule,
+  },
+});
