83104ec30070429eee00e8885b1dfde2b4b51495
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-08-09 17:48:18 +0400

    [Lesson-16] added getting data functionaliy and loading spinner

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,12 +1,10 @@
 export default {
   async registerCoach(context, data) {
-    console.log(data, 'data');
-
     const userId = context.rootGetters.userId;
     const coachData = {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      description: data.description,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
@@ -27,4 +25,29 @@ export default {
       id: userId,
     });
   },
+
+  async loadCoaches(context) {
+    const response = await fetch(`https://coaches-c1610-default-rtdb.firebaseio.com/coaches.json`);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(response.message);
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+  }
 };
