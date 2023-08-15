3fe506c862448d51540678a9697be2fc21587db0
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-08-15 14:49:55 +0400

    [Lesson-17] added store for auth

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from '@/pages/coaches/CoachDetail.vue';
-import CoachesList from '@/pages/coaches/CoachesList.vue';
-import CoachRegistration from '@/pages/coaches/CoachRegistration.vue';
-import ContactCoach from '@/pages/requests/ContactCoach.vue';
-import RequestReceived from '@/pages/requests/RequestReceived.vue';
-import NotFound from '@/pages/NotFound.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,6 +23,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
